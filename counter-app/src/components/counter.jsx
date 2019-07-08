c9a884cef068b1502979a425b69d7136cf844a6e
cnxusuk <suki.xu@tetrapak.com>
2019-07-08 09:01:50 +0800

    add react learning code

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state={
+        count:1,
+        //imageUrl:"https://picsum.photos/200"
+        tags:[]
+    };
+
+    styles={
+        fontSize: 10,
+        fontWeight: 'bold'
+    };
+
+    renderTags(){
+        if(this.state.tags.length===0) return <p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</ul>;
+    }
+
+    render() {  
+        return (
+            <React.Fragment>
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                {this.state.tags.length===0 && "Please add a tag!"}
+                {this.renderTags()}
+            </React.Fragment>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const{count}= this.state;
+        return count === 0 ? <h1>Zero</h1>: count ;
+    }
+}
+ 
+export default Counter;
