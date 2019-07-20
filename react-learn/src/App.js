d63e29b3b3c65506a5d542c16ee842cdd16f7467
cnxusuk <suki.xu@tetrapak.com>
2019-07-20 21:35:05 +0800

    addd practice

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" className ='paragragh'>
+        <navigator>Hello </navigator>
+        <h1 style={{color: "#FF8C00", backgroundColor: "#FF2D00"}}>Good Night</h1>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
       </div>
     );
-  }
+  } 
 }
 
 export default App;
