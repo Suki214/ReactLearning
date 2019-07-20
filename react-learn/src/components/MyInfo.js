c68942d981d37fd92c3077e6db970d4557adc517
cnxusuk <suki.xu@tetrapak.com>
2019-07-20 15:57:19 +0800

    add react components

---
+++ react-learn/src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function MyInfo(){
+    return(
+      <div>
+        <h1>Suki</h1>
+        <p>This is a paragraph about me, i have been these places...</p>    
+        <ul>
+          <li>China</li>
+          <li>Sweden</li>
+          <li>German</li>
+        </ul>
+    </div>
+    );
+  }
+
+
+  export default MyInfo;
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import MyInfo from './components/MyInfo'
 
-function MyApp(){
-  return(<ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>);
-}
 
 ReactDOM.render(
-<MyApp/>,
+<MyInfo/>,
 document.getElementById("root")
 )
